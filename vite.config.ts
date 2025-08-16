07cad05f4efe89acef12b6551d194f254e12b2ed
Andrey Moskokov <asm2906@gmail.com>
2025-08-16 15:42:48 +0300

    bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,6 @@ export default defineConfig({
   plugins: [react(),tailwindcss(),tsconfigPaths({
       projects: ['./tsconfig.app.json'], 
     }),],
-  base: 'alusense'
+  base: '/alusense/'
   
 })
